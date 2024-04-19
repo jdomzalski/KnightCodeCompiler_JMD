dfae7f111828df4e8c44cac8a4115597bac8dfda
jdomzalski <100503451+jdomzalski@users.noreply.github.com>
2024-04-19 18:27:23 -0400

    Update CompilerTest.java

--- compiler/CompilerTest.java
+++ compiler/CompilerTest.java
@@ -1,56 +1,91 @@
+ 
 
-package compiler;
-/**
- * This class encapsulates a basic grammar test.
- */
+package compiler; 
 
-import java.io.IOException;
-//ANTLR packages
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.gui.Trees;
+/** 
 
-import lexparse.*;
+* This class encapsulates a basic grammar test. 
 
-public class CompilerTest{
+*/ 
+
+  
+
+import java.io.IOException; 
+
+//ANTLR packages 
+
+import org.antlr.v4.runtime.*; 
+
+import org.antlr.v4.runtime.CharStream; 
+
+import org.antlr.v4.runtime.CharStreams; 
+
+import org.antlr.v4.runtime.tree.*; 
+
+import org.antlr.v4.gui.Trees; 
+
+  
+
+import lexparse.*; 
+
+public class CompilerTest{ 
 
 
-    public static void main(String[] args){
-    	
-    	if(args.length < 1) {
-    		System.out.println("Usage: java CompilerTest <input_file>");
-    		return;
-    	}
-    	
-        CharStream input;
-        KnightCodeLexer lexer;
-        CommonTokenStream tokens;
-        KnightCodeParser parser;
+    public static void main(String[] args){ 
 
-        try{
-            input = CharStreams.fromFileName(args[0]);  //get the input
-            lexer = new KnightCodeLexer(input); //create the lexer
-            tokens = new CommonTokenStream(lexer); //create the token stream
-            parser = new KnightCodeParser(tokens); //create the parser
-       
-            ParseTree tree = parser.file();  //set the start location of the parser
-             
-            
-            Trees.inspect(tree, parser);  //displays the parse tree
-            
-            //System.out.println(tree.toStringTree(parser));
+    	 
+
+    	/* 
+
+    	 * Had to add this if statement as a workaround to an error I was getting 
+
+    	 */ 
+
+    	if(args.length < 1) { 
+
+    		System.out.println("Running the CompilerTest"); 
+
+    		return; 
+
+    	}     	 
+
+        CharStream input; 
+
+        KnightCodeLexer lexer; 
+
+        CommonTokenStream tokens; 
+
+        KnightCodeParser parser; 
+
+        try{ 
+
+            input = CharStreams.fromFileName(args[0]);  //get the input 
+
+            lexer = new KnightCodeLexer(input); //create the lexer 
+
+            tokens = new CommonTokenStream(lexer); //create the token stream 
+
+            parser = new KnightCodeParser(tokens); //create the parser 
+
         
-        }
-        catch(IOException e){
-            System.out.println(e.getMessage());
-        }
+
+            ParseTree tree = parser.file();  //set the start location of the parser 
+     
+
+            Trees.inspect(tree, parser);  //displays the parse tree 
+
+             
+        } 
+
+        catch(IOException e){ 
+
+            System.out.println(e.getMessage()); 
+
+        } 
+
+    } 
 
 
-    }
+}//end class 
 
-
-
-
-}//end class
+ 
