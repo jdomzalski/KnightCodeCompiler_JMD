ec4d55e4c614e92c0032692a4e9bbe7804e038a5
jdomzalski <100503451+jdomzalski@users.noreply.github.com>
2024-04-19 18:28:13 -0400

    Update kcc.java

--- compiler/kcc.java
+++ compiler/kcc.java
@@ -21,7 +21,6 @@
 package compiler; 
 
   
-
 import org.antlr.v4.runtime.*; 
 
 import org.antlr.v4.runtime.CharStreams; 
@@ -35,7 +34,6 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.objectweb.asm.*; 
 
   
-
 import lexparse.KnightCodeLexer; 
 
 import lexparse.KnightCodeParser; 
@@ -52,27 +50,20 @@ import java.util.ArrayList;
 
 import java.util.Scanner; 
 
-  
 
 public class kcc { 
-
-  
 
 	public static void main(String[] args) throws Exception{ 
 
 		 
-
 		// Scanner to read input from user 
 
 		Scanner scan = new Scanner(System.in); 
 
 		 
-
 		// ArrayList to store input and output files 
 
 		ArrayList<String> files = new ArrayList(); 
-
-		 
 
 		// Getting the path of the input file 
 
@@ -97,42 +88,30 @@ public class kcc {
 		String outputFile = scan.nextLine(); 
 
 		files.add(outputFile); 
-
-		 
+	 
 
 		// Generate a tree and feed visitor with that tree 
 
 		// Need an ANTLR input stream (input will be input file name) 
 
 		 
-
 		CharStream input = CharStreams.fromFileName(inputFile); 
-
-		 
 
 		//Creating the lexer 
 
 		KnightCodeLexer lexer = new KnightCodeLexer(input); 
 
-		 
-
 		// Creating the tokens from the lexer 
 
 		CommonTokenStream tokens = new CommonTokenStream(lexer); 
-
-		 
 
 		// Feed parser with tokens 
 
 		KnightCodeParser parser = new KnightCodeParser(tokens); 
 
-		 
-
 		// Can then use whole thing to generate a parse tree 
 
 		ParseTree tree = parser.file(); // file is the entry rule for our grammar 
-
-		 
 
 		// Create the visitor and visit the parse tree we generated 
 
@@ -142,8 +121,6 @@ public class kcc {
 
 		byte[] bytecode = visitor.visit(tree); 
 
-		 
-
 		// Writing the output file 
 
 		try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)){ 
@@ -151,9 +128,6 @@ public class kcc {
 			fileOutputStream.write(bytecode); 
 
 		} 
-
-  
-
 	} // End main 
 
   
